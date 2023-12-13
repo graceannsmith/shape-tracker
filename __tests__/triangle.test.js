29156f9bc8a0c589881fd3af91a08ad72dae83ec
Graceann Smith <graceannsmith@gmail.com>
2023-12-13 11:52:21 -0800

    Pass jset test for isosclese triangle

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -12,4 +12,8 @@ describe('Triangle', () => {
     const notTriangle = new Triangle(3,9,22);
     expect(notTriangle.checkType()).toEqual("not a triangle");
   });
+  test('should correctly determine whether three lengths make an isosceles triangle', () => {
+    const isoscTriangle = new Triangle(5,5,7)
+    expect(isoscTriangle.checkType()).toEqual("isosceles triangle");
+  });
 });
